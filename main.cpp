7d0bf2aa92b5da9382e55c7bb8903dd78d64117a
jason <zjsd@mail.ustc.edu.cn>
2022-01-18 06:14:29 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,102 @@
-﻿#include <cstdio>
+﻿
+#include"Threadpool.h"
+#include <stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+#include<iostream>
+void fun(void* arg) {
+    int* num = (int*)arg;
+    std::cout << "the number is " << *num << std::endl;
+    sleep(1);
+}
+int main()
+{
+
+    Threadpool pool(3, 20);
+    sleep(5);
+    for (int i = 0; i < 20; i++) {
+        int* arg = (int*)malloc(sizeof(int));
+
+        *arg = i;
+        int* num = (int*)((void*)arg);
+        std::cout << "main the number is " << *num << std::endl;
+        pool.addtasktotaskq(Task(fun, num));
+        sleep(1);
+
+    }
+    std::cout << "创建完了\n";
+
+    sleep(2000);
+    return 0;}
+/*
+#include <stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include "Threadpool.h"
+#include<iostream>
+void fun(void* arg) {
+    int* num = (int*)arg;
+    std::cout << "the number is " << *num << std::endl;
+    sleep(1);
+}
+int main()
+{
+
+    ThreadPool* pool = threadpool_create(5, 10, 15, 10);
+    sleep(5);
+    for (int i = 0; i < 20; i++) {
+        int* arg = (int*)malloc(sizeof(int));
+
+        *arg = i;
+        int* num = (int*)((void*)arg);
+        std::cout << "main the number is " << *num << std::endl;
+        producer(pool, fun, (void*)arg);
+        sleep(1);
+
+    }
+    std::cout << "创建完了\n";
+    sleep(20);
+    threadpool_destroy(pool);
+    sleep(2000);
+    return 0;
+}*/
+/*#include <stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include "Threadpool.h"
+#include<iostream>
+
+
+
+
+void taskFunc(void* arg)
+{
+    int num = *(int*)arg;
+    printf("thread %ld is working, number = %d\n",
+        pthread_self(), num);
+    sleep(1);
+}
 
 int main()
 {
-    printf("%s 向你问好!\n", "ConsoleApplication1");
+    // 创建线程池
+    ThreadPool* pool = threadPoolCreate(3, 10, 100);
+    for (int i = 0; i < 100; ++i)
+    {
+        int* num = (int*)malloc(sizeof(int));
+        *num = i + 100;
+        threadPoolAdd(pool, taskFunc, num);
+
+    }
+
+    sleep(3000);
+
+    threadPoolDestroy(pool);
     return 0;
 }
+
+*/
